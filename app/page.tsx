7dfec121c9a0f7145fc68a6b941362c39a387879
yangtaizheng <694484379@qq.com>
2024-04-10 15:26:03 +0800

    init project

---
+++ app/page.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+    
+      <h1>Welcome to nextjs with deployed by AWS Amplify</h1>
+      <div className={styles.center}>
+        <Image
+          className={styles.logo}
+          src="/next.svg"
+          alt="Next.js Logo"
+          width={180}
+          height={37}
+          priority
+        />
+      </div>
+
+      
+    </main>
+  );
+}
